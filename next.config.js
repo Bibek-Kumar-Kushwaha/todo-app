0f2e0dca578ce07924d3a911b48b02f6149188aa
Bibek Kumar Kushwaha <kushwahabibek9@gmail.com>
2025-05-09 14:55:34 +0545

    fix bug 7

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+// next.config.js
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default {
   output: 'standalone',
   outputFileTracingRoot: __dirname,
   outputFileTracingExcludes: {
-    './generated/client/**': true
+    // glob '*' = every page/route
+    '*': ['./generated/client/**']
   },
   webpack: (config, { webpack }) => {
     config.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp: /__tests__/
-      })
-    );
-    return config;
+      new webpack.IgnorePlugin({ resourceRegExp: /__tests__/ })
+    )
+    return config
   },
-};
+}
